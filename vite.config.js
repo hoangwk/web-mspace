fcb6a21696f9348080fb6e705db3c3659e38b236
Hoang Le <lqhoang191@gmail.com>
2026-01-05 14:59:26 +0700

    Setup deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ghPages from "vite-plugin-gh-pages";
+// import ghPages from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  // plugins: [react(), ghPages()],
+  plugins: [react()],
   base: "/web-mspace/",
 });
